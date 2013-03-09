977191f095a269cc470fadbf4d839c0e2b81d785
phung <phungnc@gmail.com>
2013-03-10 01:01:25 +0700

    first commit

---
+++ js/models/guestModel.js
@@ -0,0 +1,18 @@
+// models/guestModel.js
+(function ( Models ) {
+
+		Models.Guest = Backbone.Model.extend({
+		// Default attributes for the guest
+		/*defaults: {
+			avatar: null,
+			name: "Nguyen Cao Phung",
+			group: "Gia dinh",
+			confirm: false,
+			number: 4
+		},*/
+
+		initialize: function(){}
+
+	});
+	
+})( App.Models );
---
+++ js/views/GuestView.js
@@ -0,0 +1,164 @@
+// view/guestView.js
+( function ( Views ) {
+
+	Views.Guest = Backbone.View.extend({
+		
+		tagName: "li",
+		//className: "a.group",
+		// get the HTML content of the #guestlist-template template
+		guestTemplate: _.template($('#guestTemplate').html()),
+
+		events:{
+			"click #guest-remove-btn" : "clear",
+			//"dblclick .fullname" : "changeName",
+			//"click .group" : "changeGroup"
+		},
+
+		initialize: function(){
+			//console.log("guestView initialize");
+			this.model.bind('change', this.render, this);
+			this.model.bind('destroy', this.remove, this);
+			this.model.bind('add',this.save,this);
+
+		},
+
+		//__group : [],
+
+		render: function(){
+			console.log("Guest render");
+			// pass model attributes to template variable
+			//console.log(this.model);
+			//this.model.attributes.number = this.model.attributes.number || 0;
+			this.$el.html(this.guestTemplate(this.model.attributes));
+
+			$(this.el.getElementsByClassName("fullname")).editable({
+					params: this,
+					mode: 'inline',
+					showbuttons: false,
+					//placement:'bottom',
+					unsavedclass: null,
+					url:this.changeName
+				});
+
+			$(this.el.getElementsByClassName("group")).editable({
+					source: App.Views.Groups,
+					//source: this.__group,
+					sourceCache: false,
+					params: this,
+					//mode: 'inline',
+					showbuttons: false,
+					placement:'bottom',
+					unsavedclass: null,
+					url:this.changeGroup
+				});
+
+			$(this.el.getElementsByClassName("confirm")).editable({
+					source: [{value: 0,text:"No"},{value:1,text:"Yes"}],
+					params: this,
+					//autotext: 'always',
+					mode: 'inline',
+					showbuttons: false,
+					inputclass: 'input-mini',
+					placement:'bottom',
+					unsavedclass: null,
+					url:this.changeConfirm
+				});
+
+			$(this.el.getElementsByClassName("number")).editable({
+					params: this,
+					mode: 'inline',
+					showbuttons: false,
+					inputclass: 'input-smini',
+					//placement:'bottom',
+					unsavedclass: null,
+					validate: function(value){
+						if($.trim(value) < 0){ 
+							return "Number must >= 0";
+						}
+					},
+					url:this.changeNumber
+				});
+
+			if(this.model){
+				groupName = this.model.attributes.group
+				var g = _.find(App.Views.Groups,function(group) {
+					return group.text == groupName;
+				},groupName );
+
+				if(!g) {;	
+					App.Views.Groups.push({value: groupName , text:groupName});	
+				}
+			} 
+		
+			return this;
+		},
+
+		getGroup: function(){
+			console.log("Guest getGroup");
+			//var group = [{value:"Company",text:"Company"},{value:"Family",text:"Family"},{value:"Buddy",text:"Buddy"}];
+			//var group = Views.appView.getGroup();
+			return this.__group;
+		},
+
+		changeName: function(params){
+			console.log("Guest changeName");
+			//params.model.set("name",params.value);
+			params.model.save({name:params.value},{silent:true});	
+			return true;
+		},		
+
+		changeNumber: function(params){
+			console.log("Guest changeNumber");
+			//params.model.set("number",params.value);
+			var num = parseInt(params.value);
+			if(num>0){
+				//params.model.set({confirm: 1, number:num},{silent: true});
+				params.model.save({confirm: 1, number:num});
+			} else {
+				//params.model.set({confirm: 0, number:num},{silent: true});
+				params.model.save({confirm: 0, number:num});
+			}
+			//params.model.save();	
+			return true;
+		},
+
+		changeConfirm: function(params){
+			console.log("Guest changeConfirm");
+			var conf = parseInt(params.value);
+			//params.model.set("confirm",bl);
+			if(conf){
+				params.model.save({confirm: 1, number:1});
+			} else {
+				params.model.save({confirm: 0, number:0});
+			}
+			//params.model.save({silent: true});
+			return true;
+		},
+
+		changeGroup: function(params){
+			console.log("Guest changeGroup");
+			/*
+			events.preventDefault();
+			var currentTarget = events.currentTarget;
+			//var seft = this;
+			$(currentTarget.parentNode.parentNode).siblings().html(currentTarget.text+'<b class="caret"></b>');
+			this.model.set("group",currentTarget.text);
+			this.model.save();
+			*/
+			//params.model.set("group",params.value);
+			params.model.save({group:params.value},{silent:true});	
+			return params.value;
+
+		},
+		remove: function(){
+			console.log("Guest remove");
+			this.$el.remove();
+		},
+
+		clear: function(){
+			console.log("Guest clear");
+			this.model.destroy();
+		},
+
+	});
+})( App.Views );
